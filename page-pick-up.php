636a7fb298f5df040529bb427260a19a4f584b17
MIYAZAKI Yuuki <yuuki@m-yuuki.com>
2022-05-13 22:30:00 +0900

    画像にsrcsetを使用しないように

--- page-pick-up.php
+++ page-pick-up.php
@@ -23,7 +23,7 @@
             ?>
                     <li>
                         <a href="<?php the_permalink(); ?>">
-                            <?php attachment_image('medium', 'img'); ?>
+                            <img src="<?php attachment_image('medium', 'url'); ?>" alt="<?php echo get_the_title().'のマクロ写真'; ?>" title="<?php echo get_the_title().'のマクロ写真'; ?>" loading="lazy">
                         </a>
                     </li>
                 <?php endwhile;?>
