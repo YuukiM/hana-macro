8566796c71f56b83acaa31135154a911e6259d06
MIYAZAKI Yuuki <yuuki@m-yuuki.com>
2022-05-14 16:24:26 +0900

    検索結果にページネーション

--- search.php
+++ search.php
@@ -8,11 +8,10 @@
             <ul class="common-image-list">
                 <?php
                     while(have_posts()): the_post();
-
                 ?>
                     <li>
                         <a href="<?php echo get_the_permalink(); ?>">
-                            <?php attachment_image('medium', 'img'); ?>
+                            <img src="<?php attachment_image('medium', 'url'); ?>" alt="<?php echo get_the_title().'のマクロ写真'; ?>" title="<?php echo get_the_title().'のマクロ写真'; ?>" width="400" height="300" loading="lazy">
                         </a>
                     </li>
                 <?php endwhile; ?>
@@ -21,6 +20,15 @@
     <?php else: ?>
         <h1 class="information__heading section-heading">検索キーワードにマッチする写真がありません</h1>
     <?php endif; ?>
+    <?php the_posts_pagination(
+        array(
+            'mid_size'      => 2, // 現在ページの左右に表示するページ番号の数
+            'prev_next'     => true, // 「前へ」「次へ」のリンクを表示する場合はtrue
+            'prev_text' => '<i class="fa-solid fa-angle-left"></i>',
+            'next_text' => '<i class="fa-solid fa-angle-right"></i>',
+            'type'          => 'list', // 戻り値の指定 (plain/list)
+        )
+    ); ?>
 </section>
 <section class="in-page-search common-section">
     <p class="in-page-search__text">
