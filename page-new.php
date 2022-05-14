a18161af6436dcc5e321dda293ea43c4dd8f4f22
MIYAZAKI Yuuki <yuuki@m-yuuki.com>
2022-05-14 13:57:39 +0900

    ページネーションの追加

--- page-new.php
+++ page-new.php
@@ -1,40 +1,47 @@
 <?php get_header(); ?>
     <section class="new-items common-section">
         <h1 class="information__heading section-heading"><?php the_title(); ?></h1>
-        <ul class="new-items__list common-image-list">
+        <?php
+            $paged = get_query_var('paged') ? get_query_var('paged') : 1 ;
+            $args = array(
+                'post_type' => 'post',
+                'posts_per_page' => 30,
+                'paged' => $paged
+            );
+            $the_query = new WP_Query( $args );
+            if ( $the_query->have_posts() ) :
+        ?>
+            <ul class="new-items__list common-image-list">
             <?php
-                $the_query = new WP_Query(
-                    array(
-                        'post_type' => 'post',
-                        'posts_per_page' => 60
-                    )
-                );
-                if ( $the_query->have_posts() ) :
-                    while ( $the_query->have_posts() ) :
-                        $the_query->the_post();
+                while ( $the_query->have_posts() ) :
+                    $the_query->the_post();
             ?>
-                    <li>
-                        <a href="<?php the_permalink(); ?>">
-                            <img src="<?php attachment_image('medium', 'url'); ?>" alt="<?php echo get_the_title().'のマクロ写真'; ?>" title="<?php echo get_the_title().'のマクロ写真'; ?>" width="400" height="300" loading="lazy">
-                        </a>
-                    </li>
-                <?php endwhile;?>
-            <?php else : ?>
-                <li>新しい記事はありません。</li>
-            <?php endif; ?>
-            <?php wp_reset_postdata(); ?>
+                <li>
+                    <a href="<?php the_permalink(); ?>">
+                        <img src="<?php attachment_image('medium', 'url'); ?>" alt="<?php echo get_the_title().'のマクロ写真'; ?>" title="<?php echo get_the_title().'のマクロ写真'; ?>" width="400" height="300" loading="lazy">
+                    </a>
+                </li>
+            <?php endwhile; ?>
         </ul>
+        <?php wp_reset_postdata(); ?>
+        <?php else: ?>
+        <p>新しい記事はありません。</p>
+
+        <?php endif; ?>
+        <?php
+            global $the_query;
+            $big = 999999999; // need an unlikely integer
+            echo paginate_links( array(
+                'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                'format' => '/page/%#%',
+                'current' => max( 1, get_query_var('paged') ),
+                'total' => $the_query->max_num_pages,
+                'type' => 'list',
+                'mid_size' => 2,
+                'prev_text' => '<i class="fa-solid fa-angle-left"></i>',
+                'next_text' => '<i class="fa-solid fa-angle-right"></i>',
+            ) );
+        ?>
+
     </section>
-    <?php /*
-        the_posts_pagination(
-            array(
-                'mid_size'      => 2, // 現在ページの左右に表示するページ番号の数
-                'prev_next'     => true, // 「前へ」「次へ」のリンクを表示する場合はtrue
-                'prev_text'     => __( '前へ'), // 「前へ」リンクのテキスト
-                'next_text'     => __( '次へ'), // 「次へ」リンクのテキスト
-                'type'          => 'list', // 戻り値の指定 (plain/list)
-            )
-        );
- */
-    ?>
 <?php get_footer(); ?>
--- page-pick-up.php
+++ page-pick-up.php
@@ -1,23 +1,27 @@
 <?php get_header(); ?>
     <section class="new-items common-section">
         <h1 class="information__heading section-heading"><?php the_title(); ?></h1>
-        <ul class="new-items__list common-image-list">
+
             <?php
-            $the_query = new WP_Query(
-                array(
-                    'post_type' => 'post',
-                    'posts_per_page' => 60,
-                    'meta_query' => array(
-                        'relation' => 'AND',
-                        array(
-                            'key' => 'pickUp',
-                            'value' => 'pickup',
-                            'compare' => 'LIKE'
-                        )
+            $paged = get_query_var('paged') ? get_query_var('paged') : 1 ;
+            $args = array(
+                'post_type' => 'post',
+                'posts_per_page' => 30,
+                'paged' => $paged,
+                'meta_query' => array(
+                    'relation' => 'AND',
+                    array(
+                        'key' => 'pickUp',
+                        'value' => 'pickup',
+                        'compare' => 'LIKE'
                     )
                 )
             );
+            $the_query = new WP_Query( $args );
             if ( $the_query->have_posts() ) :
+            ?>
+            <ul class="new-items__list common-image-list">
+            <?php
                 while ( $the_query->have_posts() ) :
                     $the_query->the_post();
             ?>
@@ -27,10 +31,25 @@
                         </a>
                     </li>
                 <?php endwhile;?>
-            <?php else : ?>
-                <li>ピックアップはありません。</li>
-            <?php endif; ?>
+            </ul>
             <?php wp_reset_postdata(); ?>
-        </ul>
+            <?php else : ?>
+                <p>ピックアップはありません。</p>
+            <?php endif; ?>
+            <?php
+            global $the_query;
+            $big = 999999999; // need an unlikely integer
+            echo paginate_links( array(
+                'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                'format' => '/page/%#%',
+                'current' => max( 1, get_query_var('paged') ),
+                'total' => $the_query->max_num_pages,
+                'type' => 'list',
+                'mid_size' => 2,
+                'prev_text' => '<i class="fa-solid fa-angle-left"></i>',
+                'next_text' => '<i class="fa-solid fa-angle-right"></i>',
+            ) );
+            ?>
+
     </section>
 <?php get_footer(); ?>
