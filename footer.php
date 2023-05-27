9e83bf50800dc3f7cf62cc7d608492f686b13fb1
MIYAZAKI Yuuki <yuuki@m-yuuki.com>
2023-05-27 18:34:35 +0900

    get_theme_file_uri

--- footer.php
+++ footer.php
@@ -37,10 +37,10 @@
     </div>
 
     <script src="https://kit.fontawesome.com/8cd2a8b7a7.js" crossorigin="anonymous"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/modaal.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/slick.min.js"></script>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
+    <script src="<?php echo get_theme_file_uri('/js/jquery.min.js'); ?>"></script>
+    <script src="<?php echo get_theme_file_uri('/js/modaal.min.js'); ?>"></script>
+    <script src="<?php echo get_theme_file_uri('/js/slick.min.js'); ?>"></script>
+    <script src="<?php echo get_theme_file_uri('/js/script.js'); ?>"></script>
     <?php wp_footer(); ?>
 </body>
 
--- header.php
+++ header.php
@@ -129,7 +129,7 @@
 <body <?php body_class(); ?>>
     <header class="header js-header">
         <h1 class="site-title">
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="<?php bloginfo('name'); ?>" width="180" height="35"></a>
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo get_theme_file_uri('/images/logo.svg'); ?>" alt="<?php bloginfo('name'); ?>" width="180" height="35"></a>
         </h1>
         <ul class="header__nav">
             <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="ホーム"><i class="fa-solid fa-house-chimney header__icon"></i><p>ホーム</p></a></li>
